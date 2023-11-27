3da0cc91eb7b5d4ccb58b2573f8550498f1e025f
christianfdev <chriis_farias@hotmail.com>
2023-11-27 13:00:29 -0300

    change in smooth scroll

--- js/script.js
+++ js/script.js
@@ -12,10 +12,14 @@ button.addEventListener('click', menuActive);
 
 // Para realizar o efeito de transição do scroll de maneira suave
 // Utilizando jquery
+
+//Comentado para testes no scroll-behavior: smooth; 
+
+/*
 var $doc = $('html, body');
 $('.scroll').click(function() {
     $doc.animate({
         scrollTop: $( $.attr(this, 'href') ).offset().top
     }, 500);
     return false;
-});
+});*/
